750513320469d223da82ca17ed388685299ccff0
Dev Jana <dev@primeacademy.io>
2018-08-02 09:48:42 -0500

    after lecture part 1

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,43 @@
 $( document ).ready( readyNow  );
 
+function newMessage(){
+    console.log( 'in newMessage' );
+    // get user input & assemble into an object
+    let objectToSend = {
+        from: $( '#nameIn' ).val(),
+        body: $( '#messageIn' ).val()
+    } // end objectToSend
+    console.log( 'sending:', objectToSend );
+    // send to server via POST
+} // end newMessage
+
 function readyNow(){
-    console.log( 'JQ' )
+    $( '#refreshButton' ).on( 'click', refreshPage );
+    $( '#sendButton' ).on( 'click', newMessage );
 } // end readyNow
+
+function refreshPage(){
+    // GET call to messages route
+    $.ajax({
+        method: 'GET',
+        url: '/messages'
+    }).then( function( response ){
+        console.log( 'back from GET call with:', response );
+        // empty output element
+        let outputElement = $( '#messagesOut' );
+        outputElement.empty();
+        // loop through messages returned 
+        for( message of response ){
+            // display each on DOM
+            let outputString = '<li>"'; 
+            outputString += message.body; 
+            outputString += '": '; 
+            outputString += '<em>' + message.from + '</em>'; 
+            outputString += '</li>'; 
+            outputElement.append( outputString );
+        } // end for
+    }).catch( function( error ){
+        // display that there was an error
+        alert( 'nope.' );
+    }) // end ajax
+} // end refreshPage
--- server/server.js
+++ server/server.js
@@ -8,8 +8,14 @@ app.use( express.static( 'server/public' ) );
 
 // globals
 const port = 5000;
+let messages = [ { from: 'mom', body: 'I love you' } ];
 
 // spin up server
 app.listen( port, ()=> {
     console.log( 'server up on:', port );
 }) // end spin up server
+
+app.get( '/messages', ( req,res )=>{
+    console.log( '/messages GET hit' );
+    res.send( messages );
+}) // end messages GET
