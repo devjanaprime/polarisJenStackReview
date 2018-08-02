d50beaa414552bd4fa16eae54b0ba4a8c8756f68
Dev Jana <dev@primeacademy.io>
2018-08-02 10:36:19 -0500

    after lecture needs readme and video

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,10 +1,31 @@
 $( document ).ready( readyNow  );
+let username = '';
+
+function loginNow(){
+    username = $( '#nameIn' ).val();
+    console.log( 'logging in:', username );
+    $( '#welcome' ).empty();
+    $( '#welcome' ).append( 'Welcome, ' + username );
+    // toggle input elements
+    $( '#login' ).hide();
+    $( '#newMessage' ).show();
+    $( '#output' ).show();
+    $( '#nameIn' ).val( '' );
+    refreshPage();
+} // end loginNow
+
+function logoutNow(){
+    username = '';
+    $( '#login' ).show();
+    $( '#newMessage' ).hide();
+    $( '#output' ).hide();
+} // end logout
 
 function newMessage(){
     console.log( 'in newMessage' );
     // get user input & assemble into an object
     let objectToSend = {
-        from: $( '#nameIn' ).val(),
+        from: username,
         message: $( '#messageIn' ).val()
     } // end objectToSend
     console.log( 'sending:', objectToSend );
@@ -15,12 +36,18 @@ function newMessage(){
         data: objectToSend
     }).then( function( response ){
         console.log( 'back from POST with:', response );
+        $( '#messageIn' ).val( '' );
+        refreshPage();
     }).catch( function( error ){
         alert( 'problem!' );
     }) // end ajax
 } // end newMessage
 
 function readyNow(){
+    $( '#newMessage' ).hide();
+    $( '#output' ).hide();
+    $( '#loginButton' ).on( 'click', loginNow );
+    $( '#logoutButton' ).on( 'click', logoutNow );
     $( '#refreshButton' ).on( 'click', refreshPage );
     $( '#sendButton' ).on( 'click', newMessage );
 } // end readyNow
